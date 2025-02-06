f4853dac1f99ccdab1eced50e2cf4727011e9021
Sun Yi <739507690@qq.com>
2025-02-06 17:22:35 +0800

    初始化

---
+++ src/components/circle-loading/index.tsx
@@ -0,0 +1,7 @@
+export default function CircleLoading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-16 h-16 border-4 border-t-4 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+  )
+}
