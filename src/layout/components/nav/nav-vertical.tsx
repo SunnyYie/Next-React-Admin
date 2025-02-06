7dd3490cd89dd4f8c7f69f4624901cb909ecadf4
Sun Yi <739507690@qq.com>
2025-02-06 17:48:44 +0800

    feat:新增layout，新增全局样式配置

---
+++ src/layout/components/nav/nav-vertical.tsx
@@ -0,0 +1,7 @@
+export default function NavVertical() {
+  return (
+    <div className="bg-gray-100">
+      <h1 className="text-2xl font-bold text-center mt-4">Next React Admin</h1>
+    </div>
+  )
+}
